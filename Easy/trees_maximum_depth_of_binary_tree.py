38b85677d872413a355441ab0bf503ae118a5ab7
dosatos <lovetocodekz@gmail.com>
2018-04-07 18:15:39 +0100

    binary tree depth count_bfs added

--- Easy/trees_maximum_depth_of_binary_tree.py
+++ Easy/trees_maximum_depth_of_binary_tree.py
@@ -59,4 +59,22 @@ class Solution(object):
 #         return self.traverse(root, depth)
     def maxDepth(self, root):
         return 1 + max(map(self.maxDepth, (root.left, root.right))) if root else 0
-        
+    # BFS:
+    # def maxDepth(self, root):
+    #     """
+    #     :type root: TreeNode
+    #     :rtype: int
+    #     """
+    #     depth = 0
+    #     level = [root] if root else []
+    #     while level:
+    #         depth += 1
+    #         queue = []
+    #         for el in level:
+    #             if el.left:
+    #                 queue.append(el.left)
+    #             if el.right:
+    #                 queue.append(el.right)
+    #         level = queue
+            
+    #     return depth
