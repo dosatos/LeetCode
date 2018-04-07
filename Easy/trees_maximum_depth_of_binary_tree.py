0885cec35661f6b4efc3ab052bc26c3f1f169860
dosatos <lovetocodekz@gmail.com>
2018-04-07 18:20:25 +0100

    binary tree depth count_bfs added_ improved performance to 74 percentile

--- Easy/trees_maximum_depth_of_binary_tree.py
+++ Easy/trees_maximum_depth_of_binary_tree.py
@@ -33,48 +33,44 @@ Test cases:
 
 
 class Solution(object):
-#     def traverse(self, root, depth):
-#         # print(root.val, depth)
-#         l, r = None, None
-#         if not root.left and not root.right:
-#             return depth + 1
-#         if root.left:
-#             l = self.traverse(root.left, depth) + 1
-#         if root.right:
-#             r = self.traverse(root.right, depth) + 1
-#         if l:
-#             if r:
-#                 return max(l, r)
-#             return l
-#         return r
+    def traverse(self, root, depth):
+        if not root.left and not root.right:
+            return depth + 1
+        l = self.traverse(root.left, depth) + 1 if root.left else depth
+        r = self.traverse(root.right, depth) + 1 if root.right else depth
+        return max(r, l)
         
+        
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        depth = 0
+        return self.traverse(root, depth)
+    
+    # def maxDepth(self, root):
+    #     return 1 + max(map(self.maxDepth, (root.left, root.right))) if root else 0
+    
+    # BFS:
 #     def maxDepth(self, root):
 #         """
 #         :type root: TreeNode
 #         :rtype: int
 #         """
-#         if not root:
-#             return 0
 #         depth = 0
-#         return self.traverse(root, depth)
-    def maxDepth(self, root):
-        return 1 + max(map(self.maxDepth, (root.left, root.right))) if root else 0
-    # BFS:
-    # def maxDepth(self, root):
-    #     """
-    #     :type root: TreeNode
-    #     :rtype: int
-    #     """
-    #     depth = 0
-    #     level = [root] if root else []
-    #     while level:
-    #         depth += 1
-    #         queue = []
-    #         for el in level:
-    #             if el.left:
-    #                 queue.append(el.left)
-    #             if el.right:
-    #                 queue.append(el.right)
-    #         level = queue
+#         level = [root] if root else []
+#         while level:
+#             depth += 1
+#             queue = []
+#             for el in level:
+#                 if el.left:
+#                     queue.append(el.left)
+#                 if el.right:
+#                     queue.append(el.right)
+#             level = queue
             
-    #     return depth
+#         return depth
+        
