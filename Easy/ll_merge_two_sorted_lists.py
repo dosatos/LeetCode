5cd1c73192e7ceedebec25e51a96662ea93cb80a
yb2n17@soton.ac.uk <yb2n17@soton.ac.uk>
2018-06-14 21:24:55 +0100

    easy ll - merge two sorted list Another Fancy Swapping Solution

--- Easy/ll_merge_two_sorted_lists.py
+++ Easy/ll_merge_two_sorted_lists.py
@@ -32,24 +32,48 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        node1, node2 = l1, l2
-        head = ListNode(0)
-        node = head
-        while node1 and node2:
-            if node1.val <= node2.val:
-                tmp = node1.next # save tmp
-                node.next = node1 # append
-                node = node.next # increment
-                node.next = None # clean up node
-                node1 = tmp # use tmp
-            else:
-                tmp = node2.next
-                node.next = node2
-                node = node.next
-                node.next = None
-                node2 = tmp
-        if node1:
-            node.next = node1
-        else:
-            node.next = node2
-        return head.next
+        # node1, node2 = l1, l2
+        # head = ListNode(0)
+        # node = head
+        # while node1 and node2:
+        #     if node1.val <= node2.val:
+        #         tmp = node1.next # save tmp
+        #         node.next = node1 # append
+        #         node = node.next # increment
+        #         node.next = None # clean up node
+        #         node1 = tmp # use tmp
+        #     else:
+        #         tmp = node2.next
+        #         node.next = node2
+        #         node = node.next
+        #         node.next = None
+        #         node2 = tmp
+        # if node1:
+        #     node.next = node1
+        # else:
+        #     node.next = node2
+        # return head.next
+    
+#     def mergeTwoLists(self, l1, l2):
+#         """
+#         :type l1: ListNode
+#         :type l2: ListNode
+#         :rtype: ListNode
+#         """
+#         if not l1:
+#             return l2
+#         if not l2:
+#             return l1
+        
+#         if l1.val < l2.val:
+#             l1.next = self.mergeTwoLists(l1.next, l2)
+#             return l1
+#         else:
+#             l2.next = self.mergeTwoLists(l2.next, l1)
+#             return l2
+    def mergeTwoLists(self, a, b):
+        if not a or b and a.val > b.val:
+            a, b = b, a
+        if a:
+            a.next = self.mergeTwoLists(a.next, b)
+        return a
