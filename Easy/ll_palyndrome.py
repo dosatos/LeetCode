754b4ebd1e98e9e2eaf78fe060eca4ad5ce97258
yb2n17@soton.ac.uk <yb2n17@soton.ac.uk>
2018-06-16 22:01:46 +0100

    LL palindrome, Space O(1), Time O(N)

--- Easy/ll_palyndrome.py
+++ Easy/ll_palyndrome.py
@@ -20,6 +20,16 @@ to check for polyndrome
 Time Comlpexity: O(n)
 Space Comlpexity: O(n)
 
+Solution 2:
+Percentile: 76% <<< Slower, but more memory efficient
+
+Find len
+Reverse the second half of the LL
+Check for palindrome the first half to the second (reversed) half
+
+Time Comlpexity: O(n)
+Space Comlpexity: O(1)
+
 """
 # Definition for singly-linked list.
 # class ListNode:
@@ -28,16 +38,54 @@ Space Comlpexity: O(n)
 #         self.next = None
 
 class Solution:
+    # def isPalindrome(self, head):
+    #     """
+    #     :type head: ListNode
+    #     :rtype: bool
+    #     """
+    #     node = head
+    #     container = []
+    #     while node:
+    #         container.append(node.val)
+    #         node = node.next
+    #     if container == container[::-1]:
+    #         return True
+    #     return False
+        
     def isPalindrome(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
+        """ 
+        Time complexity = O(N)
+        Space complexity = O(1)
         """
         node = head
-        container = []
+        length = 0
+        # count the len of the ll
         while node:
-            container.append(node.val)
             node = node.next
-        if container == container[::-1]:
-            return True
-        return False
+            length += 1
+        
+        # initialise first half head
+        first_head = head
+        node = head
+        # 0 1 2 3 4 5 (6 // 2 = 3)
+        # 0 1 2 3 4 5 6 (7 // 2 = 3)
+        for _ in range(length // 2):
+            node = node.next
+        
+        # skip the middle if odd
+        if length % 2 != 0:
+            node = node.next
+        
+        # reverse the second half
+        prev_node = None
+        while node:
+            node.next, prev_node, node = prev_node, node, node.next
+        second_head = prev_node
+            
+        # compare the two halves
+        for _ in range(length // 2):
+            if first_head.val != second_head.val:
+                return False
+            first_head = first_head.next
+            second_head = second_head.next
+        return True
